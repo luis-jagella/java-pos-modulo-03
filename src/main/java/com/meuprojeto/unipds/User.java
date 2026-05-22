6c88164a789902335d5ef1f0304363559b59bb1a
Luís <luisprinces950@gmail.com>
2026-05-21 23:17:03 -0300

    feat: implementação inicial da classe User.

--- src/main/java/com/meuprojeto/unipds/User.java
+++ src/main/java/com/meuprojeto/unipds/User.java
@@ -1,4 +1,15 @@
 package com.meuprojeto.unipds;
 
 public class User {
+
+    public Long id;
+    public String nome;
+
+    public User() {
+    }
+
+    public User(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
 }

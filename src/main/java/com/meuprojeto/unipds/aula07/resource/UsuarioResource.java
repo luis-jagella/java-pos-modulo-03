a8267c435143a4e1e229171569d1701ca7124eb0
Luís <luisprinces950@gmail.com>
2026-05-24 17:20:30 -0300

    feat: implementa usuario resource & service

---
+++ src/main/java/com/meuprojeto/unipds/aula07/resource/UsuarioResource.java
@@ -0,0 +1,35 @@
+package com.meuprojeto.unipds.aula07.resource;
+
+import com.meuprojeto.unipds.aula07.entity.Usuario;
+import com.meuprojeto.unipds.aula07.service.UsuarioService;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+
+import java.util.List;
+
+@Path("/usuarios")
+public class UsuarioResource {
+
+    @Inject
+    UsuarioService service;
+
+    @GET
+    public List<Usuario> listar() {
+
+        return service.listar();
+    }
+
+    @POST
+    public Usuario salvar(Usuario usuario) {
+
+        return service.salvar(usuario);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public boolean remover(@PathParam("id") Long id) {
+
+        return service.remover(id);
+    }
+}
---
+++ src/main/java/com/meuprojeto/unipds/aula07/service/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.meuprojeto.unipds.aula07.service;
+
+import com.meuprojeto.unipds.aula07.entity.Usuario;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@ApplicationScoped
+public class UsuarioService {
+
+    public List<Usuario> listar() {
+        return Usuario.listAll();
+    }
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+
+        usuario.persist();
+
+        return usuario;
+    }
+
+    @Transactional
+    public boolean remover(Long id) {
+
+        return Usuario.deleteById(id);
+    }
+}

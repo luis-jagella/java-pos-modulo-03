8ead8f03984fbaead437ec46a65129fa84485934
Luís <luisprinces950@gmail.com>
2026-05-24 17:20:13 -0300

    feat: implementa usuario

---
+++ src/main/java/com/meuprojeto/unipds/aula07/entity/Usuario.java
@@ -0,0 +1,11 @@
+package com.meuprojeto.unipds.aula07.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Usuario extends PanacheEntity {
+
+    public String nome;
+
+}

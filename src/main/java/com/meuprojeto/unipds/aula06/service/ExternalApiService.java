7fc77899886b50c7fb7071e98bc7e98ad93bb0c1
Luís <luisprinces950@gmail.com>
2026-05-24 16:56:33 -0300

    feat: implementa classe exemplo para o fault

--- src/main/java/com/meuprojeto/unipds/aula06/service/ExternalApiService.java
+++ src/main/java/com/meuprojeto/unipds/aula06/service/ExternalApiService.java
@@ -1,4 +1,26 @@
 package com.meuprojeto.unipds.aula06.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
+@ApplicationScoped
 public class ExternalApiService {
+
+    @Retry(maxRetries = 3)
+    @Timeout(2000)
+    @Fallback(fallbackMethod = "retornoPadrao")
+    public String buscarDados() throws InterruptedException {
+
+        System.out.println("Tentando acessar API externa...");
+
+        Thread.sleep(3000);
+
+        throw new RuntimeException("API indisponível");
+    }
+
+    public String retornoPadrao() {
+        return "Serviço temporariamente indisponível";
+    }
 }

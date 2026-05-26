a5b34f4ffb6fc8b7904eeb343c89912f667e297b
Luís <luisprinces950@gmail.com>
2026-05-25 22:50:17 -0300

    feat: implementa classe de observabilidade com o OpenTelemetry

---
+++ src/main/java/com/meuprojeto/unipds/aula09/resource/ObservabilityResource.java
@@ -0,0 +1,20 @@
+package com.meuprojeto.unipds.aula09.resource;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/observability")
+public class ObservabilityResource {
+
+    @GET
+    public String monitorar() throws InterruptedException {
+
+        System.out.println("Iniciando processamento...");
+
+        Thread.sleep(1000);
+
+        System.out.println("Finalizando processamento");
+
+        return "Processamento concluído";
+    }
+}

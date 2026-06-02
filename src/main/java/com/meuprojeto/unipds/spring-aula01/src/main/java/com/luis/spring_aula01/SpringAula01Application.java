e62e72e43138e78a5daf39352d1bdae0edf25448
Luís <luisprinces950@gmail.com>
2026-06-01 23:56:20 -0300

    chore: adiciona estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/meuprojeto/unipds/spring-aula01/src/main/java/com/luis/spring_aula01/SpringAula01Application.java
@@ -0,0 +1,13 @@
+package com.luis.spring_aula01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAula01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAula01Application.class, args);
+	}
+
+}

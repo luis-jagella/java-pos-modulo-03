7f41a1afc55a5df3078f3893d5b8ff49407d8cc7
Luís <luisprinces950@gmail.com>
2026-06-01 23:56:43 -0300

    feat: adiciona aplicação principal Spring Boot

---
+++ src/main/java/com/meuprojeto/unipds/spring-aula01/src/test/java/com/luis/spring_aula01/SpringAula01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.spring_aula01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAula01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7794011039a58cf7687e5bf26300c3136fe23a6d
Luís <luisprinces950@gmail.com>
2026-06-02 23:44:02 -0300

    feat: adiciona controller do usuario

---
+++ spring-aula01/src/main/java/com/luis/spring_aula01/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.luis.spring_aula01.controller;
+
+import com.luis.spring_aula01.model.Usuario;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final List<Usuario> usuarios = new ArrayList<>();
+
+    @GetMapping
+    public List<Usuario> listar() {
+
+        return usuarios;
+    }
+
+    @PostMapping
+    public Usuario cadastrar(@RequestBody Usuario usuario) {
+
+        usuarios.add(usuario);
+
+        return usuario;
+    }
+}

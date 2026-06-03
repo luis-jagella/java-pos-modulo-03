7ebeb05ea197128c8a4c32def7bf593198fc95c4
Luís <luisprinces950@gmail.com>
2026-06-02 23:43:19 -0300

    feat: adiciona classe de usuario no model

---
+++ spring-aula01/src/main/java/com/luis/spring_aula01/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.luis.spring_aula01.model;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}

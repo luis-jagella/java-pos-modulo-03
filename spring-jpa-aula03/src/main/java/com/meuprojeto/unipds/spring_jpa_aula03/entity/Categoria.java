aafa87f35dea07cc52ce55a2dd50c44897086cfb
Luís <luisprinces950@gmail.com>
2026-06-22 23:55:21 -0300

    feat: implementa categoria entity

---
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/entity/Categoria.java
@@ -0,0 +1,38 @@
+package com.meuprojeto.unipds.spring_jpa_aula03.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}

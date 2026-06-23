f4224dbac6fc06430531074ead65cbc184030ccf
Luís <luisprinces950@gmail.com>
2026-06-22 23:46:29 -0300

    feat: inicializa projeto Spring Boot para estudos de JPA

---
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/SpringJpaAula03Application.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.unipds.spring_jpa_aula03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAula03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAula03Application.class, args);
+	}
+
+}
---
+++ spring-jpa-aula03/src/test/java/com/meuprojeto/unipds/spring_jpa_aula03/SpringJpaAula03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.unipds.spring_jpa_aula03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAula03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

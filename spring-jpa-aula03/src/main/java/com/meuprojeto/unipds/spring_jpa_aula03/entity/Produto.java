37df1e1c09210ea682454b036a5c0eb7e06f5eb0
Luís <luisprinces950@gmail.com>
2026-06-23 23:41:57 -0300

    feat: implementa produto FK

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/entity/Produto.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/entity/Produto.java
@@ -1,4 +1,63 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.entity;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Produto() {
+    }
+
+    public Produto(Long id, String nome, BigDecimal preco, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }

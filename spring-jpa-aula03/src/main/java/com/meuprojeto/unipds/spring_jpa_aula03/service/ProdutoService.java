e6c1e8641350d70d58c3b4ed58e478cca9ac1684
Luís <luisprinces950@gmail.com>
2026-06-29 23:29:33 -0300

    feat: implementa classe de service por produto

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/service/ProdutoService.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/service/ProdutoService.java
@@ -1,4 +1,55 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.service;
 
+import com.meuprojeto.unipds.spring_jpa_aula03.dto.ProdutoRequest;
+import com.meuprojeto.unipds.spring_jpa_aula03.dto.ProdutoResponse;
+import com.meuprojeto.unipds.spring_jpa_aula03.entity.Categoria;
+import com.meuprojeto.unipds.spring_jpa_aula03.entity.Produto;
+import com.meuprojeto.unipds.spring_jpa_aula03.repository.CategoriaRepository;
+import com.meuprojeto.unipds.spring_jpa_aula03.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+    private final CategoriaRepository categoriaRepository;
+
+    public ProdutoService(
+            ProdutoRepository produtoRepository,
+            CategoriaRepository categoriaRepository
+    ) {
+        this.produtoRepository = produtoRepository;
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public Produto salvar(ProdutoRequest request) {
+
+        Categoria categoria = categoriaRepository
+                .findById(request.getCategoriaId())
+                .orElseThrow();
+
+        Produto produto = new Produto();
+
+        produto.setNome(request.getNome());
+        produto.setPreco(request.getPreco());
+        produto.setCategoria(categoria);
+
+        return produtoRepository.save(produto);
+    }
+
+    public List<ProdutoResponse> listar() {
+
+        return produtoRepository.findAll()
+                .stream()
+                .map(produto -> new ProdutoResponse(
+                        produto.getId(),
+                        produto.getNome(),
+                        produto.getPreco(),
+                        produto.getCategoria().getNome()
+                ))
+                .collect(Collectors.toList());
+    }
 }

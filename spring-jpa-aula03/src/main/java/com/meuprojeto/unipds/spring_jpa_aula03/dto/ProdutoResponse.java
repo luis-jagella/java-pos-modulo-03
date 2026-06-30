c04ba4b021fdc6206348bb06a85a70f3382ede7b
Luís <luisprinces950@gmail.com>
2026-06-29 23:28:38 -0300

    feat: implementa classe response pra produto

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/dto/ProdutoResponse.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/dto/ProdutoResponse.java
@@ -1,4 +1,53 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.dto;
 
+import java.math.BigDecimal;
+
 public class ProdutoResponse {
+
+    private Long id;
+    private String nome;
+    private BigDecimal preco;
+    private String categoria;
+
+    public ProdutoResponse() {
+    }
+
+    public ProdutoResponse(Long id, String nome, BigDecimal preco, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }

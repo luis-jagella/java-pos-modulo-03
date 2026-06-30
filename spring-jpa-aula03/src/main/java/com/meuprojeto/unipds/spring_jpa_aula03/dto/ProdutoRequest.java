c01890f444f7c37cdac3260d5b4b0ef92a42ce43
Luís <luisprinces950@gmail.com>
2026-06-29 23:28:04 -0300

    feat: implementa classe de requisição de produto

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/dto/ProdutoRequest.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/dto/ProdutoRequest.java
@@ -1,4 +1,37 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.dto;
 
+import java.math.BigDecimal;
+
 public class ProdutoRequest {
+
+    private String nome;
+    private BigDecimal preco;
+    private Long categoriaId;
+
+    public ProdutoRequest() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Long getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(Long categoriaId) {
+        this.categoriaId = categoriaId;
+    }
 }

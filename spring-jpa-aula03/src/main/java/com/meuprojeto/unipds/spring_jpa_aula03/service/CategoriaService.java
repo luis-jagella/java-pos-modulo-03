57a8a1f199a1dfa93c280b3ae37c6f2d26dbca9c
Luís <luisprinces950@gmail.com>
2026-06-29 23:29:07 -0300

    feat: implementa classe de service para categoria

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/service/CategoriaService.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/service/CategoriaService.java
@@ -1,4 +1,25 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.service;
 
+import com.meuprojeto.unipds.spring_jpa_aula03.entity.Categoria;
+import com.meuprojeto.unipds.spring_jpa_aula03.repository.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoriaService {
+
+    private final CategoriaRepository repository;
+
+    public CategoriaService(CategoriaRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Categoria> listar() {
+        return repository.findAll();
+    }
+
+    public Categoria salvar(Categoria categoria) {
+        return repository.save(categoria);
+    }
 }

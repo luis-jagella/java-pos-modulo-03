83635b5d20d9b0471bcdc07795e49381570eb477
Luís <luisprinces950@gmail.com>
2026-07-03 23:47:05 -0300

    refactor: utiliza camada de services nos controllers

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/controller/ProdutoController.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/controller/ProdutoController.java
@@ -1,4 +1,30 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.controller;
 
+import com.meuprojeto.unipds.spring_jpa_aula03.dto.ProdutoRequest;
+import com.meuprojeto.unipds.spring_jpa_aula03.dto.ProdutoResponse;
+import com.meuprojeto.unipds.spring_jpa_aula03.entity.Produto;
+import com.meuprojeto.unipds.spring_jpa_aula03.service.ProdutoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<ProdutoResponse> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public Produto cadastrar(@RequestBody ProdutoRequest request) {
+        return service.salvar(request);
+    }
 }

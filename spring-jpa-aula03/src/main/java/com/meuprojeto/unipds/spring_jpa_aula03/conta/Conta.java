729a050fc2d7fb3fbb2c56705831019fa60891ac
Luís <luisprinces950@gmail.com>
2026-07-08 23:55:38 -0300

    feat: implementa conta

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/conta/Conta.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/conta/Conta.java
@@ -1,4 +1,58 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.conta;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "contas")
 public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titular;
+
+    private BigDecimal saldo;
+
+    public Conta() {
+    }
+
+    public Conta(Long id, String titular, BigDecimal saldo) {
+        this.id = id;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void debitar(BigDecimal valor) {
+        this.saldo = this.saldo.subtract(valor);
+    }
+
+    public void creditar(BigDecimal valor) {
+        this.saldo = this.saldo.add(valor);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
 }

a9ba1b0c74d46f02255651f5cb2343eb1b3df24d
Luís <luisprinces950@gmail.com>
2026-07-11 23:51:48 -0300

    refactor: refatora classe de transferencia

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/transferencia/TransferenciaRequest.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/transferencia/TransferenciaRequest.java
@@ -1,4 +1,43 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.transferencia;
 
+import java.math.BigDecimal;
+
 public class TransferenciaRequest {
+
+    private Long contaOrigemId;
+    private Long contaDestinoId;
+    private BigDecimal valor;
+    private boolean simularErro;
+
+    public Long getContaOrigemId() {
+        return contaOrigemId;
+    }
+
+    public void setContaOrigemId(Long contaOrigemId) {
+        this.contaOrigemId = contaOrigemId;
+    }
+
+    public Long getContaDestinoId() {
+        return contaDestinoId;
+    }
+
+    public void setContaDestinoId(Long contaDestinoId) {
+        this.contaDestinoId = contaDestinoId;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public boolean isSimularErro() {
+        return simularErro;
+    }
+
+    public void setSimularErro(boolean simularErro) {
+        this.simularErro = simularErro;
+    }
 }

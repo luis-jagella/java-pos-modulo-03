ce5262183ae96fc894afad8c0cc43879f54d1b94
Luís <luisprinces950@gmail.com>
2026-07-12 22:31:27 -0300

    refactor: melhoria na classe de exception SaldoInsuficienteException

--- spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/exception/SaldoInsuficienteException.java
+++ spring-jpa-aula03/src/main/java/com/meuprojeto/unipds/spring_jpa_aula03/exception/SaldoInsuficienteException.java
@@ -1,4 +1,8 @@
 package com.meuprojeto.unipds.spring_jpa_aula03.exception;
 
-public class SaldoInsuficienteException {
+public class SaldoInsuficienteException extends RuntimeException {
+
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
 }

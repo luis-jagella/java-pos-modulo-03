751b06b674181066b3378fbe86a52122bae3c2cc
Luís <luisprinces950@gmail.com>
2026-07-16 23:58:41 -0300

    test: adiciona comentário para fins de testes.

--- spring-jpa-aula03/src/test/java/com/meuprojeto/unipds/spring_jpa_aula03/SpringJpaAula03ApplicationTests.java
+++ spring-jpa-aula03/src/test/java/com/meuprojeto/unipds/spring_jpa_aula03/SpringJpaAula03ApplicationTests.java
@@ -3,6 +3,8 @@ package com.meuprojeto.unipds.spring_jpa_aula03;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+// test
 @SpringBootTest
 class SpringJpaAula03ApplicationTests {
 
